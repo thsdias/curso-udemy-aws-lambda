0558d89ff4887941027aee63e14e2c683d54ca4d
thsdias <thdias.84@gmail.com>
2025-10-06 20:24:39 -0300

    Section 3 - Persisting Data

--- AWSLambda/Function.cs
+++ AWSLambda/Function.cs
@@ -1,3 +1,5 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -7,16 +9,18 @@ namespace AWSLambda;
 
 public class Function
 {
-
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(User input, ILambdaContext context)
+    public async Task<User> FunctionHandler(Guid input, ILambdaContext context)
     {
-        return input.Name.ToUpper();
+        var dynamoDbContext = new DynamoDBContext(new AmazonDynamoDBClient());
+        var user = await dynamoDbContext.LoadAsync<User>(input);
+
+        return user;
     }
 }
 
